d2e737710d382175de89640a2bc3aa5c416572e2
“praveenbk94” <praveenbalakrishnan94@gmail.com>
2023-03-12 10:47:29 +0000

    Third commit

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -43,5 +43,10 @@ printPattern(n);
 1
 2
 1
-2
+2Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the value of n: ");  
+int n = sc.nextInt();  
+System.out.println();  
+//function calling  
+printPattern(n); 
 22
